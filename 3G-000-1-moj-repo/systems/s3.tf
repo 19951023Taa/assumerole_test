ee7f143b41f307d7e3460d3db24d59262e44e017
19951023Taa <takamaaa777@gmail.com>
2023-02-28 11:53:59 +0900

    s3policy_jsonを作成

--- 3G-000-1-moj-repo/systems/s3.tf
+++ 3G-000-1-moj-repo/systems/s3.tf
@@ -2,5 +2,12 @@ module "s3_terraform_main" {
   count  = var.ENV == "pd" ? 1 : 0
   source = "../modules/s3"
 
-  bucket_name = "3g-000-1-terraform-bucket"
+  bucket_name = "terraform.s3.tfstate"
+  policy           = templatefile(
+    "${path.module}/s3_bucket_policy/terraform_bucket_policy.json" ,
+    {
+      bucket_name  = "terraform.s3.tfstate", 
+      account_id   = "${data.aws_caller_identity.this.account_id}"
+    }
+  )
 }
