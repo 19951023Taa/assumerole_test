1f208a188ca27daaaa77bfe9f4ca1147ca738881
19951023Taa <takamaaa777@gmail.com>
2023-02-28 22:53:52 +0900

    rds_instance作成

--- 3G-000-1-moj-repo/systems/rds.tf
+++ 3G-000-1-moj-repo/systems/rds.tf
@@ -1,28 +1,23 @@
 module "rds_cluster_main" {
-  source = "../modules/rds_cluster"
+  source = "../modules/rds/rds_cluster"
 
   cluster_identifier     = "${local.PRO}-${local.SYS}-${var.ENV}-rds-cluster-01"
 # vpc_security_group_ids = [module.securitygroups["sg_rds_main_psql"].group_id]
   engine                 = "aurora-postgresql"
   engine_version         = "14.6"
   port                   = 5432
-  database_name          = var.database_name
-  master_username        = var.master_username
-  master_password        = var.master_password
-
-  instance_class         = var.instance_class
-  parameter_group_name    = "${local.PRO}-${local.SYS}-${var.ENV}-db-cluster-parametergroup-01"
-  db_parameter_group_name    = "${local.PRO}-${local.SYS}-${var.ENV}-db-instance-parametergroup-01"
-  storage_encrypted       = true
+  storage_encrypted      = true
   backup_retention_period = 1
-  preferred_backup_window           = "16:00-16:30"
+  preferred_backup_window   = "16:00-16:30"
   enabled_cloudwatch_logs_exports = [
     "postgresql"
   ]
-  auto_minor_version_upgrade = false
   preferred_maintenance_window         = "Sat:17:00-Sat:17:30"
   deletion_protection        = false
 
+  database_name          = var.database_name
+  master_username        = var.master_username
+  master_password        = var.master_password
 
   subnet_ids = [
     module.vpc_subnets["private_subnet_a_01"].subnet_id,
@@ -31,4 +26,21 @@ module "rds_cluster_main" {
 
   family = "aurora-postgresql14"
   db_subnet_group_name = "${local.PRO}-${local.SYS}-${var.ENV}-db-subnetgroup-01"
+  db_parameter_group_name    = "${local.PRO}-${local.SYS}-${var.ENV}-db-instance-parametergroup-01"
+  parameter_group_name    = "${local.PRO}-${local.SYS}-${var.ENV}-db-cluster-parametergroup-01"
+}
+
+
+module "rds_instance_main" {
+  source = "../modules/rds/rds_instance"
+  count = 2
+
+  cluster_identifier     = module.rds_cluster_main.cluster_identifier
+  identifier         = "${local.PRO}-${local.SYS}-${var.ENV}-rds-instance-0${count.index + 1}"
+  engine             = module.rds_cluster_main.engine
+  engine_version     = module.rds_cluster_main.engine_version
+  instance_class         = var.instance_class
+  db_subnet_group_name    = module.rds_cluster_main.subnet_group_name
+  db_parameter_group_name = module.rds_cluster_main.parameter_group_name
+  auto_minor_version_upgrade = false
 }
