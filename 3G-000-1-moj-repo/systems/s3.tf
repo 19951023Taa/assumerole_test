3dd58ec132beac1e102c62f404699075b723fda9
19951023Taa <takamaaa777@gmail.com>
2023-03-19 14:56:34 +0900

    test

--- 3G-000-1-moj-repo/systems/s3.tf
+++ 3G-000-1-moj-repo/systems/s3.tf
@@ -1,8 +1,7 @@
 module "s3_terraform_main" {
-  count  = var.ENV == "pd" ? 1 : 0
   source = "../modules/s3"
 
-  bucket_name = "terraform.s3.tfstate.pd.st"
+  bucket_name = "terraform.s3.tfstate.${var.ENV}"
   policy = templatefile(
     "${path.module}/s3_bucket_policy/terraform_bucket_policy.json",
     {
